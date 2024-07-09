fa5c22d734ba79fd3603691a76d1061b3a6e5b48
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-07-09 16:15:12 +0900

    Time: 154 ms (40.00%), Space: 41.6 MB (32.13%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,53 +1,54 @@
+import java.nio.MappedByteBuffer;
+
 class Solution {
 
-	public boolean exist(char[][] board, String word) {
-		int n = board.length;
-		int m = board[0].length;
-		int index = 0;
+	int n;
+	int m;
+	int[] dr = new int[] {-1, 1, 0, 0};
+	int[] dc = new int[] {0, 0, -1, 1};
 
+	public boolean exist(char[][] board, String word) {
+		this.n = board.length;
+		this.m = board[0].length;
+
+		StringBuilder answer = new StringBuilder();
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (board[i][j] == word.charAt(0)) {
-					char buffer = board[i][j];
+					char value = board[i][j];
 					board[i][j] = '.';
-					if (backtrack(board, word, i, j, index + 1))
+					if (backtrack(i, j, board, word, answer, 1)) {
 						return true;
-					board[i][j] = buffer;
+					}
+					board[i][j] = value;
+					;
 				}
 			}
 		}
-
 		return false;
 	}
 
-	private boolean backtrack(char[][] board, String word, int x, int y, int index) {
+	private boolean backtrack(int r, int c, char[][] board, String word, StringBuilder sb, int index) {
+
 		if (index == word.length()) {
 			return true;
 		}
 
-		int n = board.length;
-		int m = board[0].length;
-
-		int[] dr = {-1, 1, 0, 0};
-		int[] dc = {0, 0, -1, 1};
-
 		for (int i = 0; i < 4; i++) {
-			int nextX = x + dr[i];
-			int nextY = y + dc[i];
-			if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m) {
-				if (board[nextX][nextY] == word.charAt(index)) {
-					char buffer = board[nextX][nextY];
-					board[nextX][nextY] = '.';
-
-					if (backtrack(board, word, nextX, nextY, index + 1)) {
+			int nextR = r + dr[i];
+			int nextC = c + dc[i];
+			if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < m) {
+				if (board[nextR][nextC] == word.charAt(index)) {
+					char value = board[nextR][nextC];
+					board[nextR][nextC] = '.';
+					if (backtrack(nextR, nextC, board, word, sb, index + 1)) {
 						return true;
 					}
-					board[nextX][nextY] = buffer;
+					board[nextR][nextC] = value;
 				}
 			}
 		}
 
 		return false;
 	}
-
 }
