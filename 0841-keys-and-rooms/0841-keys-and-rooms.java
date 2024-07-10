f3c1b00547362812a1e98e9a4041cd3da482e602
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-07-11 01:53:03 +0900

    Time: 16 ms (5.07%), Space: 44.3 MB (35.31%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+
+	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+		Map<Integer, List<Integer>> graph = new HashMap<>();
+		for (int i = 0; i < rooms.size(); i++) {
+			graph.putIfAbsent(i, new ArrayList<>());
+			graph.put(i, rooms.get(i));
+		}
+		int startVertex = 0;
+		return bfs(startVertex, graph, new ArrayList<>()) == rooms.size();
+	}
+
+	private int bfs(int startVertex, Map<Integer, List<Integer>> graph, List<Integer> visited) {
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(startVertex);
+		visited.add(startVertex);
+
+		while (!queue.isEmpty()) {
+			int currentVertex = queue.poll();
+			for (int vertex : graph.get(currentVertex)) {
+				if (!visited.contains(vertex)) {
+					queue.add(vertex);
+					visited.add(vertex);
+				}
+			}
+		}
+
+		return visited.size();
+	}
+}
