c669566218bd28e667f9f4e56a935115146891fc
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-07-10 23:16:24 +0900

    Time: 666 ms (14.23%), Space: 44.6 MB (14.42%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -8,41 +8,31 @@ class Solution {
 
 	public String getPermutation(int n, int k) {
 		visitied = new boolean[n + 1];
-		String result = backtracking(n, k, new ArrayList<>());
-		if(!result.isEmpty()){
-			return result;
-		}
-		return "NO";
+		backtracking(n, k, new ArrayList<>());
+		return answer;
 	}
 
-	private String backtracking(int n, int k, List<String> list) {
+	private void backtracking(int n, int k, List<String> list) {
 
 		if(list.size() == n){
 			count += 1;
 			if(count == k){
-				return String.join("", list);
+				answer = String.join("", list);
 			}
 		}
 
 		if(count > k){
-			return null;
+			return;
 		}
-
-
 
 		for(int i = 1; i <= n; i++){
 			if(!visitied[i]) {
 				list.add(String.valueOf(i));
 				visitied[i] = true;
-				String result = backtracking(n, k, list);
-				if(result != null){
-					return result;
-				}
+				backtracking(n, k, list);
 				visitied[i] = false;
 				list.remove(list.size() - 1);
 			}
 		}
-
-		return null;
 	}
 }
