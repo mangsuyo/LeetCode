819c8e522ba6a3f5ac43d2b777a946cbd15be014
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-08-19 14:59:42 +0900

    Time: 35 ms (82.58%), Space: 63.3 MB (5.21%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,20 +1,21 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
 
 class Solution {
-
 	public int[] dailyTemperatures(int[] temperatures) {
 		Deque<List<Integer>> stack = new ArrayDeque<>();
-
 		int[] answer = new int[temperatures.length];
 
-		for(int i = 0; i < temperatures.length; i++){
+		for (int i = 0; i < temperatures.length; i++) {
 			int temperature = temperatures[i];
-			while(!stack.isEmpty() && stack.peek().get(0) < temperature){
-				List<Integer> temperatureInfo = stack.pop();
-				answer[temperatureInfo.get(1)] = i - temperatureInfo.get(1);
+			while((!stack.isEmpty()) && (stack.peek().get(1) < temperature)){
+				List<Integer> item = stack.pop();
+				answer[item.get(0)] = i - item.get(0);
 			}
-			stack.push(Arrays.asList(temperature, i));
+			stack.push(List.of(i, temperature));
 		}
+		
 		return answer;
 	}
 }
