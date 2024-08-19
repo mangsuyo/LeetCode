53f2397fc9452ef3fa79e8422523116740d7a26c
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-08-19 13:50:43 +0900

    Time: 1 ms (98.77%), Space: 41.5 MB (33.33%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,30 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
+	public boolean isValid(String s) {
+		Deque<Character> stack = new ArrayDeque<>();
 
-    public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+		for(int i = 0; i < s.length(); i++){
+			if(s.charAt(i) == '('){
+				stack.push(')');
+			}
+			else if(s.charAt(i) == '['){
+				stack.push(']');
+			}
+			else if(s.charAt(i) == '{'){
+				stack.push('}');
+			}
+			else{
+				if(stack.isEmpty()){
+					return false;
+				}
+				if(stack.pop() != s.charAt(i)){
+					return false;
+				}
+			}
+		}
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(') {
-                stack.push(')');
-            } else if (ch == '{') {
-                stack.push('}');
-            } else if (ch == '[') {
-                stack.push(']');
-            } else {
-                if (stack.isEmpty() || stack.pop() != ch) {
-                    return false;
-                }
-            }
-        }
-        if (!stack.isEmpty())
-            return false;
-        return true;
-    }
+		return stack.isEmpty();
+	}
 }
