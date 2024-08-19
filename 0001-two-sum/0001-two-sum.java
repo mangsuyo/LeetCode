fb071a70095ea5553b9e4d3574dbfe7f3fbbd914
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-08-19 13:28:50 +0900

    Time: 8 ms (54.49%), Space: 44 MB (99.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,33 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
 class Solution {
 	public int[] twoSum(int[] nums, int target) {
-		for(int i = 0; i < nums.length; i++){
-			for(int j = i + 1; j < nums.length; j++){
-				if(nums[i] + nums[j] == target){
-					return new int[]{i, j};
-				}
+
+		int[][] indexAndNumber = new int[nums.length][2];
+		for (int i = 0; i < nums.length; i++) {
+				indexAndNumber[i][0] = nums[i];
+				indexAndNumber[i][1] = i;
+		}
+
+		Arrays.sort(indexAndNumber, (a, b) -> Integer.compare(a[0], b[0]));
+
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left < right) {
+			int sum = indexAndNumber[left][0] + indexAndNumber[right][0];
+			if (sum < target) {
+				left += 1;
+			} else if (sum > target) {
+				right -= 1;
+			} else {
+				return new int[] {indexAndNumber[left][1], indexAndNumber[right][1]};
 			}
 		}
-	return new int[]{0};
+
+		return new int[0];
 	}
 }
