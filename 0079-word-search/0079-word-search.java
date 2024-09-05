08b30065a85443b51c94b72db2de9b6cb3504c38
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-09-05 14:13:23 +0900

    Time: 143 ms (61.22%), Space: 41.4 MB (80.31%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,52 +1,46 @@
-import java.nio.MappedByteBuffer;
-
 class Solution {
 
 	int n;
 	int m;
-	int[] dr = new int[] {-1, 1, 0, 0};
-	int[] dc = new int[] {0, 0, -1, 1};
+	String target;
+
+	int[] dr = {-1, 1, 0, 0};
+	int[] dc = {0, 0, -1, 1};
 
 	public boolean exist(char[][] board, String word) {
-		this.n = board.length;
-		this.m = board[0].length;
+		n = board.length;
+		m = board[0].length;
+		target = word;
 
-		StringBuilder answer = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (board[i][j] == word.charAt(0)) {
-					char value = board[i][j];
-					board[i][j] = '.';
-					if (backtrack(i, j, board, word, answer, 1)) {
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				if(board[i][j] == word.charAt(0)){
+					if(dfs(board, i, j, 0)){
 						return true;
-					}
-					board[i][j] = value;
-					;
+					};
 				}
 			}
 		}
+
 		return false;
 	}
 
-	private boolean backtrack(int r, int c, char[][] board, String word, StringBuilder sb, int index) {
-
-		if (index == word.length()) {
+	boolean dfs(char[][] board, int i, int j, int count){
+		if(count == target.length() - 1){
 			return true;
 		}
 
-		for (int i = 0; i < 4; i++) {
-			int nextR = r + dr[i];
-			int nextC = c + dc[i];
-			if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < m) {
-				if(board[nextR][nextC] != '.') {
-					if (board[nextR][nextC] == word.charAt(index)) {
-						char value = board[nextR][nextC];
-						board[nextR][nextC] = '.';
-						if (backtrack(nextR, nextC, board, word, sb, index + 1)) {
-							return true;
-						}
-						board[nextR][nextC] = value;
-					}
+		for(int k = 0; k < 4; k++){
+			int nextR = i + dr[k];
+			int nextC = j + dc[k];
+			if(nextR >= 0 && nextR < n && nextC >= 0 && nextC < m){
+				if(board[nextR][nextC] == target.charAt(count + 1)){
+					char ch = board[i][j];
+					board[i][j] = '.';
+					if(dfs(board, nextR, nextC, count + 1)){
+						return true;
+					};
+					board[i][j] = ch;
 				}
 			}
 		}
