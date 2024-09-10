a8720bb3f83d98cc59c15e68df6ade2e47e0b266
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-09-11 08:48:26 +0900

    Time: 2 ms (53.89%), Space: 44.6 MB (16.50%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -2,24 +2,22 @@ import java.util.*;
 
 class Solution {
 	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+		Queue<Integer> queue = new ArrayDeque<>();
 		Set<Integer> visited = new HashSet<>();
+
+		queue.add(0);
 		visited.add(0);
-		dfs(rooms, 0, visited);
 
-		System.out.println(visited.size());
-
-		return visited.size() == rooms.size();
-	}
-
-	void dfs(List<List<Integer>> rooms, int vertex, Set<Integer> visited){
-		List<Integer> room = rooms.get(vertex);
-
-		for(Integer key: room){
-			if(!visited.contains(key)){
-				visited.add(key);
-				dfs(rooms, key, visited);
+		while (!queue.isEmpty()) {
+			int cur = queue.poll();
+			for (Integer key : rooms.get(cur)) {
+				if (!visited.contains(key)) {
+					queue.add(key);
+					visited.add(key);
+				}
 			}
 		}
-
+		
+		return visited.size() == rooms.size();
 	}
 }
