885e478380e0cec3de4a2ef1b87bdf7e971a98fe
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-09-11 10:20:16 +0900

    Time: 1 ms (63.93%), Space: 44.8 MB (81.47%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,57 +1,58 @@
 import java.util.*;
 
-class Pair<T1, T2> {
-    T1 first;
-    T2 second;
-
-    public Pair(T1 first, T2 second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    @Override
-    public String toString() {
-        return "(" + first + ", " + second + ")";
-    }
-}
-
 class Solution {
-    public boolean isBipartite(int[][] graph) {
-        char[] chars = new char[graph.length];
-        Arrays.fill(chars, 'N'); // 초기화 ('N'는 아직 방문하지 않은 노드)
+	class Fair {
+		int first;
+		char second;
 
-        for (int i = 0; i < graph.length; i++) {
-            if (chars[i] == 'N') { // 아직 방문하지 않은 노드에 대해 BFS 수행
-                if (!bfs(graph, i, 'A', chars)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+		public Fair(int first, char second) {
+			this.first = first;
+			this.second = second;
+		}
 
-    boolean bfs(int[][] graph, int startVertex, char ch, char[] chars) {
-        Queue<Pair<Integer, Character>> queue = new ArrayDeque<>();
-        chars[startVertex] = ch;
-        queue.add(new Pair<>(startVertex, ch));
+		@Override
+		public String toString() {
+			return "Fair{" +
+				"first=" + first +
+				", second=" + second +
+				'}';
+		}
+	}
 
-        while (!queue.isEmpty()) {
-            Pair<Integer, Character> cur = queue.poll();
+	public boolean isBipartite(int[][] graph) {
+		char[] chars = new char[graph.length];
+		Arrays.fill(chars, 'N');
 
-            for (int neighbor : graph[cur.first]) {
-                if (chars[neighbor] == 'N') { // 아직 방문하지 않은 경우
-                    if (cur.second == 'A') {
-                        queue.add(new Pair<>(neighbor, 'B'));
-                        chars[neighbor] = 'B';
-                    } else {
-                        queue.add(new Pair<>(neighbor, 'A'));
-                        chars[neighbor] = 'A';
-                    }
-                } else if (chars[neighbor] == cur.second) {
-                    return false; // 현재 노드와 이웃 노드가 같은 그룹이면 이분 그래프가 아님
-                }
-            }
-        }
-        return true;
-    }
+		for(int i = 0; i < graph.length; i++){
+			if(chars[i] == 'N'){
+				if(!bfs(graph, chars, i, 'A')){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	boolean bfs(int[][] graph, char[] chars, int start, char ch) {
+		Queue<Fair> queue = new ArrayDeque<>();
+		queue.add(new Fair(start, ch));
+
+		while (!queue.isEmpty()) {
+			Fair fair = queue.poll();
+			for (Integer key : graph[fair.first]) {
+				if (chars[key] == 'N') {
+					if (fair.second == 'A') {
+						queue.add(new Fair(key, 'B'));
+						chars[key] = 'B';
+					} else {
+						queue.add(new Fair(key, 'A'));
+						chars[key] = 'A';
+					}
+				} else if (chars[key] == fair.second) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
