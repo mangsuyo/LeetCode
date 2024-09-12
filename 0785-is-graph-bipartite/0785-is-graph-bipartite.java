15044fb28a6b98f83d6d65d4eef25a2006d531fb
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-09-12 11:38:20 +0900

    Time: 6 ms (17.33%), Space: 44.8 MB (67.58%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,58 +1,52 @@
 import java.util.*;
 
 class Solution {
-	class Fair {
-		int first;
-		char second;
-
-		public Fair(int first, char second) {
-			this.first = first;
-			this.second = second;
-		}
-
-		@Override
-		public String toString() {
-			return "Fair{" +
-				"first=" + first +
-				", second=" + second +
-				'}';
-		}
-	}
+	// class Fair {
+	// 	int first;
+	// 	char second;
+	//
+	// 	public Fair(int first, char second) {
+	// 		this.first = first;
+	// 		this.second = second;
+	// 	}
+	//
+	// 	@Override
+	// 	public String toString() {
+	// 		return "Fair{" +
+	// 			"first=" + first +
+	// 			", second=" + second +
+	// 			'}';
+	// 	}
+	// }
 
 	public boolean isBipartite(int[][] graph) {
 		char[] chars = new char[graph.length];
 		Arrays.fill(chars, 'N');
 
-		for(int i = 0; i < graph.length; i++){
-			if(chars[i] == 'N'){
-				if(!bfs(graph, chars, i, 'A')){
-					return false;
-				}
+		for(int i = 0 ; i < graph.length; i++){
+			if(chars[i] == 'N') {
+				if(!dfs(i, graph, chars, 'A')) return false;
+			}
+		}
+
+		for(int i = 0; i < chars.length; i++){
+			System.out.println(chars[i]);
+		}
+
+		return true;
+	}
+
+	boolean dfs(int index, int[][] graph, char[] chars, char ch) {
+		chars[index] = ch;
+		for (int key : graph[index]) {
+			if (chars[key] == 'N') {
+					if (!dfs(key, graph, chars, ch == 'A' ? 'B' : 'A'))
+						return false;
+			} else if(chars[key] == ch) {
+				return false;
 			}
 		}
 		return true;
 	}
 
-	boolean bfs(int[][] graph, char[] chars, int start, char ch) {
-		Queue<Fair> queue = new ArrayDeque<>();
-		queue.add(new Fair(start, ch));
-
-		while (!queue.isEmpty()) {
-			Fair fair = queue.poll();
-			for (Integer key : graph[fair.first]) {
-				if (chars[key] == 'N') {
-					if (fair.second == 'A') {
-						queue.add(new Fair(key, 'B'));
-						chars[key] = 'B';
-					} else {
-						queue.add(new Fair(key, 'A'));
-						chars[key] = 'A';
-					}
-				} else if (chars[key] == fair.second) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
 }
