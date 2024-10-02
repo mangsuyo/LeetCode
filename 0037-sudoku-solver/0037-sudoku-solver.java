05b5ed54479be20ae52a9436c58fefeaa45568ed
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-10-02 10:20:51 +0900

    Time: 5 ms (87.15%), Space: 40.9 MB (70.44%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,56 +1,76 @@
+import java.util.Arrays;
+
 class Solution {
 
-	int n = 9;
-	int m = 9;
+	int n;
+	int m;
 
 	public void solveSudoku(char[][] board) {
-		backtrack(board);
-		System.out.println(board);
+		this.n = board.length;
+		this.m = board[0].length;
+
+		char[][] clone = new char[n][m];
+
+		for (int i = 0; i < n; i++) {
+			clone[i] = board[i].clone();
+		}
+
+		backtrack(clone, 0);
+
+		for (int i = 0; i < n; i++) {
+			board[i] = clone[i].clone();
+		}
 	}
 
-	private boolean backtrack(char[][] board){
+	public boolean backtrack(char[][] clone, int count) {
 
+		if (count == 81)
+			return true;
 
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				if(board[i][j] == '.'){
-					for(int k = 1; k <= n; k++){
-						char value = Character.forDigit(k, 10);
-						if(inspect(i, j, value, board)){
-							board[i][j] = value;
-							if(backtrack(board)){
-								return true;
-							}
-							else{
-								board[i][j] = '.';
-							}
-						}
-					}
+		int r = count / 9;
+		int c = count % 9;
+
+		if (clone[r][c] != '.')
+			return backtrack(clone, count + 1);
+		else {
+			for (int i = 1; i <= 9; i++) {
+				char nextValue = (char)('0' + i);
+				if (isValid(clone, nextValue, r, c)) {
+					clone[r][c] = nextValue;
+					if (backtrack(clone, count + 1))
+						return true;
+					clone[r][c] = '.';
+				}
+			}
+		}
+		return false;
+	}
+
+	boolean isValid(char[][] board, char nextValue, int row, int col) {
+
+		for (int i = 0; i < 9; i++) {
+			if (board[row][i] == nextValue) {
+				return false;
+			}
+		}
+
+		for (int i = 0; i < 9; i++) {
+			if (board[i][col] == nextValue) {
+				return false;
+			}
+		}
+
+		int r = row / 3;
+		int c = col / 3;
+
+		for (int i = r * 3; i < r * 3 + 3; i++) {
+			for (int j = c * 3; j < c * 3 + 3; j++) {
+				if (board[i][j] == nextValue) {
 					return false;
 				}
 			}
 		}
-		return true;
-	}
 
-	private boolean inspect(int r, int c, char number, char[][] board){
-
-		int startR = (r / 3) * 3;
-		int startC = (c / 3) * 3;
-
-		for(int i = 0; i < n; i++){
-			if(board[r][i] == number) return false;
-		}
-
-		for(int i = 0; i < n; i++){
-			if(board[i][c] == number) return false;
-		}
-
-		for(int i = startR; i < startR + 3; i++){
-			for(int j = startC; j < startC + 3; j++){
-				if(board[i][j] == number) return false;
-			}
-		}
 		return true;
 	}
 }
