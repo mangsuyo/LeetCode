ff769ca091683777c7226fe6686b2eb94fbb42f2
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-09-01 21:27:59 +0900

    Time: 3 ms (4.82%), Space: 44.6 MB (5.73%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,31 +1,22 @@
-import java.util.*;
-
 class Solution {
-	public int minCostClimbingStairs(int[] cost) {
-		int n = cost.length;
-		int[] memo = new int[n + 1];
-		int[] stairs = new int[n + 1];
 
-		for (int i = 0; i < cost.length; i++) {
-			stairs[i] = cost[i];
-		}
+    Map<Integer, Integer> memo = new HashMap<>();
+    public int minCostClimbingStairs(int[] cost) {
+        return dp(cost, cost.length);
+    }
 
-		stairs[n] = 0;
+    int dp(int[] cost, int n){
+        if(n == 0) return 0;
+        if(n == 1) return 0;
 
-		Arrays.fill(memo, -1);
-		return dp(memo, stairs, n);
-	}
+        if(!memo.containsKey(n)){
+            memo.put(n, Math.min(cost[n - 1] + dp(cost, n - 1), cost[n - 2] + dp(cost, n - 2)));
+        }
 
-	int dp(int[] memo, int[] cost, int stair) {
-		if (stair == 0)
-			return cost[0];
-		if (stair == 1)
-			return cost[1];
+        return memo.get(n);
 
-		if (memo[stair] == -1) {
-			memo[stair] = cost[stair] + Math.min(dp(memo, cost, stair - 1), dp(memo, cost, stair - 2));
-		}
+    }
 
-		return memo[stair];
-	}
+        
+
 }
