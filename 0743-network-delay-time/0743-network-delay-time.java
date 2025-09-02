ce260159256122c60dde506b85fb7fe3bec6a398
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-09-03 08:06:37 +0900

    Time: 14 ms (41.79%), Space: 47.7 MB (88.93%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,45 +1,46 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        Map<Integer, List<int[]>> edges = new HashMap<>();
-        for (int[] time : times) {
-            edges.putIfAbsent(time[0], new ArrayList<>());
-            edges.get(time[0]).add(new int[]{time[1], time[2]});
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for(int[] time: times){
+            graph.putIfAbsent(time[0], new ArrayList<>());
+            graph.get(time[0]).add(new int[]{time[1], time[2]});
         }
 
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[k] = 0;
+        int[] dists = new int[n + 1];
+        Arrays.fill(dists, Integer.MAX_VALUE);
+        dists[k] = 0;
 
-        Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
-        pq.add(new int[]{k, 0});
+        Queue<int[]> queue = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
+        queue.offer(new int[]{k, 0});
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int cur_v = cur[0];
-            int cur_t = cur[1];
+        int count = 1;
 
-            if (cur_t > dist[cur_v]) continue;
-            if (!edges.containsKey(cur_v)) continue;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int curNode = cur[0];
+            int curDist = cur[1];
 
-            for (int[] edge : edges.get(cur_v)) {
-                int next_v = edge[0];
-                int next_t = edge[1];
+            if(dists[curNode] < curDist) continue;
 
-                if (next_t + dist[cur_v] < dist[next_v]) {
-                    dist[next_v] = next_t + dist[cur_v];
-                    pq.add(new int[]{next_v, dist[next_v]});
+            if(graph.containsKey(curNode)){
+                for(int[] next: graph.get(curNode)){
+                    if(dists[next[0]] == Integer.MAX_VALUE) count += 1;
+                    int nextDist = dists[curNode] + next[1];
+                    if(nextDist < dists[next[0]]){
+                        queue.offer(new int[]{next[0], nextDist});
+                        dists[next[0]] = nextDist;
+                    }
                 }
             }
         }
 
-        int maxTime = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == Integer.MAX_VALUE) {
-                return -1; 
-            }
-            maxTime = Math.max(maxTime, dist[i]);
-        }
+        if(count != n) return -1;
         
+        int maxTime = 0;
+        for(int i = 1; i < dists.length; i++){
+            if(maxTime < dists[i]) maxTime = dists[i];
+        }
+
         return maxTime;
     }
 }
