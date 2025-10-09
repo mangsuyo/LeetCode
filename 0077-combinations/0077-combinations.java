866165fbf71e422c965f70f52fdd88d907bc2a38
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-10-10 06:31:44 +0900

    Time: 17 ms (86.16%), Space: 94.7 MB (60.69%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,23 +1,21 @@
-import java.util.*;
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> answer = new ArrayList<>();
+        backtrack(n, k, 1, new ArrayList<>(), answer);
+        return answer;
+    }
 
-public class Solution {
-	public List<List<Integer>> combine(int n, int k) {
-		List<List<Integer>> answer = new ArrayList<>();
-		backtracking(1, n, k, new ArrayList<>(), answer);
-		return answer;
-	}
+    void backtrack(int n, int k, int start, List<Integer> list, List<List<Integer>> answer){
 
-	private void backtracking(int start, int n, int k, List<Integer> list, List<List<Integer>> answer) {
-		if(list.size() == k){
-			answer.add(new ArrayList<>(list));
-			return;
-		}
+        if(list.size() == k){
+            answer.add(new ArrayList<>(list));
+            return;
+        }
 
-		for(int i = start; i <= n; i++){
-			System.out.println(list);
-			list.add(i);
-			backtracking(i + 1, n, k, list, answer);
-			list.remove(list.size() - 1);
-		}
-	}
+        for(int i = start; i <= n; i++){
+            list.add(i);
+            backtrack(n, k, i + 1, list, answer);
+            list.remove(list.size() - 1);
+        }
+    }
 }
