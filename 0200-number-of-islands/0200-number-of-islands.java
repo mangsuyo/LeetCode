c16fcb56a862c7acc5580b71a41990123d966f41
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-10-10 07:12:39 +0900

    Time: 6 ms (23.32%), Space: 49 MB (93.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,48 +1,56 @@
-import java.util.*;
-
 class Solution {
-	public int numIslands(char[][] grid) {
 
-		boolean[][] visited = new boolean[grid.length][grid[0].length];
-		int answer = 0;
+    int n;
+    int m;
+    boolean[][] visited;
 
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[0].length; j++) {
-				if (grid[i][j] == '1' && !visited[i][j]) {
-					bfs(i, j, grid, visited);
-					answer += 1;
-				}
-			}
-		}
+    int[] dr = new int[]{-1, 1, 0, 0};
+    int[] dc = new int[]{0, 0, -1, 1};
 
-		return answer;
-	}
+    public int numIslands(char[][] grid) {
+        this.n = grid.length;
+        this.m = grid[0].length;
+        visited = new boolean[n][m];
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                char c = grid[i][j];
+                if(!visited[i][j] && c == '1'){
+                    bfs(grid, i, j);
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
 
-	public void bfs(int i, int j, char[][] grid, boolean[][] visited) {
+    void bfs(char[][] grid, int r, int c){
+        Queue<Pair> queue = new ArrayDeque<>();
+        queue.offer(new Pair(r, c));
+        visited[r][c] = true;
 
-		visited[i][j] = true;
-		Queue<List<Integer>> queue = new ArrayDeque<>();
-		queue.add(List.of(i, j));
+        while(!queue.isEmpty()){
+            Pair cur = queue.poll();
+            for(int i = 0; i < 4; i++){
+                int nextR = cur.r + dr[i];
+                int nextC = cur.c + dc[i];
+                if(0 <= nextR && nextR < n && 0 <= nextC && nextC < m){
+                    if(!visited[nextR][nextC] && grid[nextR][nextC] == '1'){
+                        queue.offer(new Pair(nextR, nextC));
+                        visited[nextR][nextC] = true;
+                    }
+                }
+            }
+        }
+    }
+}
 
-		int[] dr = {-1, 1, 0, 0};
-		int[] dc = {0, 0, -1, 1};
-
-		while(!queue.isEmpty()) {
-			List<Integer> cur = queue.poll();
-			int curR = cur.get(0);
-			int curC = cur.get(1);
-
-			for (int k = 0; k < 4; k++) {
-				int nextR = curR + dr[k];
-				int nextC = curC + dc[k];
-				if (0 <= nextR && nextR < grid.length && 0 <= nextC && nextC < grid[0].length) {
-					if (!visited[nextR][nextC] && grid[nextR][nextC] == '1') {
-						queue.add(List.of(nextR, nextC));
-						visited[nextR][nextC] = true;
-					}
-				}
-			}
-		}
-
-	}
+class Pair{
+    int r;
+    int c;
+    
+    public Pair(int r, int c){
+        this.r = r;
+        this.c = c;
+    }
 }
