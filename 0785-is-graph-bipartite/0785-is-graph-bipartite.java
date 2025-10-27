34f642e6cacbbb5e515cd879ea044fdba3940ae9
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-10-27 09:26:45 +0900

    Time: 2 ms (32.8%), Space: 45 MB (68.86%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,44 +1,31 @@
 class Solution {
     int n;
+
     public boolean isBipartite(int[][] graph) {
         this.n = graph.length;
-        char[] nodes = new char[n];
-
-        Arrays.fill(nodes, '.');
+        int[] nodes = new int[n];
+        Arrays.fill(nodes, -1);
 
         for(int i = 0; i < n; i++){
-            if(nodes[i] == '.'){
+            if(nodes[i] == -1){
                 Queue<Integer> queue = new ArrayDeque<>();
                 queue.offer(i);
-                nodes[i] = 'A';
+                nodes[i] = 0;
                 while(!queue.isEmpty()){
-                    int curNode = queue.poll();
-                    char nextCh = nodes[curNode] == 'A' ? 'B' : 'A'; 
-                    for(int nextNode: graph[curNode]){
-                        if(nodes[nextNode] == '.'){
+                    int node = queue.poll();
+                    int nextNum = nodes[node] == 0 ? 1 : 0;
+                    for(int nextNode: graph[node]){
+                        if(nodes[nextNode] == -1){
                             queue.offer(nextNode);
-                            nodes[nextNode] = nextCh;
+                            nodes[nextNode] = nextNum;
                         }
-                        else if(nodes[nextNode] == nodes[curNode]) return false;
+                        else{
+                            if(nodes[nextNode] == nodes[node]) return false;
+                        }
                     }
                 }
             }
         }
-
-
-        return true;
-    }
-
-    boolean dfs(int[][] graph, char[] nodes, char ch, int node){
-        nodes[node] = ch;
-        char nextCh = ch == 'A' ? 'B' : 'A';
-        for(int nextNode: graph[node]){
-            if(nodes[nextNode] == '.'){
-                if(!dfs(graph, nodes, nextCh, nextNode)) return false;
-            }
-            else if(nodes[nextNode] == ch) return false;
-        }
-
         return true;
     }
 }
