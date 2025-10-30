80ccc0da22b31360d16c8a4c839c3ad1f4653293
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-10-30 19:18:52 +0900

    Time: 14 ms (42.77%), Space: 47.4 MB (92.55%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,16 +1,17 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
-        for(int[] time: times){
-            graph.putIfAbsent(time[0], new ArrayList<>());
-            graph.get(time[0]).add(new int[]{time[1], time[2]});
+
+        for(int[] t: times){
+            graph.putIfAbsent(t[0], new ArrayList<>());
+            graph.get(t[0]).add(new int[]{t[1], t[2]});
         }
 
         int[] dists = new int[n + 1];
         Arrays.fill(dists, Integer.MAX_VALUE);
         dists[k] = 0;
 
-        Queue<int[]> queue = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
+        Queue<int[]> queue = new PriorityQueue<>((n1, n2) -> n1[1] - n2[1]);
         queue.offer(new int[]{k, 0});
 
         int count = 1;
@@ -19,17 +20,16 @@ class Solution {
             int[] cur = queue.poll();
             int curNode = cur[0];
             int curDist = cur[1];
-            if(curDist > dists[curNode]) continue;
-
+            if(dists[curNode] < curDist) continue;
+            
             if(graph.containsKey(curNode)){
                 for(int[] next: graph.get(curNode)){
-                    if(dists[next[0]] == Integer.MAX_VALUE){
-                        count += 1;
-                    }
+                    int nextNode = next[0];
                     int nextDist = curDist + next[1];
-                    if(nextDist < dists[next[0]]){
-                        queue.offer(new int[]{next[0], nextDist});
-                        dists[next[0]] = nextDist;
+                    if(dists[nextNode] == Integer.MAX_VALUE) count++;
+                    if(dists[nextNode] > nextDist){
+                        queue.offer(new int[]{nextNode, nextDist});
+                        dists[nextNode] = nextDist;
                     }
                 }
             }
@@ -41,7 +41,6 @@ class Solution {
         for(int i = 1; i <= n; i++){
             answer = Math.max(answer, dists[i]);
         }
-
         return answer;
     }
 }
