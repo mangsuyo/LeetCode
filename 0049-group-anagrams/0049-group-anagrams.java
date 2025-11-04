21ba4809958fe69beb7d4d134c7678c028578621
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-11-04 17:43:00 +0900

    Time: 7 ms (62.48%), Space: 49.3 MB (11.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,49 +1,23 @@
 class Solution {
+    int n;
+
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> list = new ArrayList<>();
-        boolean[] visited = new boolean[strs.length];
-        for(int i = 0; i < strs.length; i++){
-            List<String> temp = new ArrayList<>();
-            if(!visited[i]){
-                temp.add(strs[i]);
-                visited[i] = true;
-            }
-            for(int j = i + 1; j < strs.length; j++){
-                if(strs[i].length() != strs[j].length()) continue;
-                else{
-                    int flag = 1;
-                    for(Character c: strs[i].toCharArray()){
-                        if(!strs[j].contains(String.valueOf(c))){
-                            flag = 0;
-                            break;
-                        }
-                        if(getCount(strs[j], c) != getCount(strs[i], c)){
-                            flag = 0;
-                            break;
-                        }
-                    }
-                    if(flag == 1 && !visited[j]){
-                        temp.add(strs[j]);
-                        visited[j] = true;
-                    }
-                }
-            }
-            if(!temp.isEmpty()){
-                list.add(new ArrayList<>(temp));
-            }
+        this.n = strs.length;
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            String cur = strs[i];
+            char[] chars = cur.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            map.putIfAbsent(sorted, new ArrayList<>());
+            map.get(sorted).add(cur);
+        }
+        List<List<String>> answer = new ArrayList<>();
+        for(List<String> list: map.values()){
+            answer.add(list);
         }
 
-        System.out.println(list);
-        return list;
-    }
-
-    int getCount(String str, Character target){
-        int count = 0;
-        for(Character c: str.toCharArray()){
-            if(c == target){
-                count += 1;
-            }
-        }
-        return count;
+        return answer;
     }
 }
