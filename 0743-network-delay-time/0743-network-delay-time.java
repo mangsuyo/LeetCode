e50a7df404851311c7a2199d1462eab8e106e708
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-11-24 20:03:21 +0900

    Time: 14 ms (48.52%), Space: 50.2 MB (12.26%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,18 +1,17 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
-
-        for(int[] t: times){
-            graph.putIfAbsent(t[0], new ArrayList<>());
-            graph.get(t[0]).add(new int[]{t[1], t[2]});
+        for(int[] time: times){
+            graph.putIfAbsent(time[0], new ArrayList<>());
+            graph.get(time[0]).add(new int[]{time[1], time[2]});
         }
 
         int[] dists = new int[n + 1];
         Arrays.fill(dists, Integer.MAX_VALUE);
-        dists[k] = 0;
 
-        Queue<int[]> queue = new PriorityQueue<>((n1, n2) -> n1[1] - n2[1]);
+        Queue<int[]> queue = new PriorityQueue<>((p1, p2) -> p1[1] - p2[1]);
         queue.offer(new int[]{k, 0});
+        dists[k] = 0;
 
         int count = 1;
 
@@ -20,27 +19,28 @@ class Solution {
             int[] cur = queue.poll();
             int curNode = cur[0];
             int curDist = cur[1];
-            if(dists[curNode] < curDist) continue;
-            
+            if(curDist > dists[curNode]) continue;
+
             if(graph.containsKey(curNode)){
                 for(int[] next: graph.get(curNode)){
                     int nextNode = next[0];
-                    int nextDist = curDist + next[1];
-                    if(dists[nextNode] == Integer.MAX_VALUE) count++;
-                    if(dists[nextNode] > nextDist){
-                        queue.offer(new int[]{nextNode, nextDist});
+                    int nextDist = next[1] + curDist;
+                    if(dists[nextNode] == Integer.MAX_VALUE){
+                        count += 1;
+                    }
+                    if(nextDist < dists[nextNode]){
                         dists[nextNode] = nextDist;
+                        queue.offer(new int[]{nextNode, nextDist});
                     }
                 }
             }
-        }
-
-        if(count != n) return -1;
+        }   
 
         int answer = 0;
         for(int i = 1; i <= n; i++){
             answer = Math.max(answer, dists[i]);
         }
-        return answer;
+
+        return count == n ? answer : -1;
     }
 }
