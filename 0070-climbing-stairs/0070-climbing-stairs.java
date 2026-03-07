c1dacf9520a3eb28ffa8a7856b0ca618cdeb3db1
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2026-03-07 12:36:45 +0900

    Time: 0 ms (100%), Space: 42.4 MB (14.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,17 @@
 class Solution {
-
-    int[] memo;
-
+    Map<Integer, Integer> hash = new HashMap<>();
     public int climbStairs(int n) {
-        memo = new int[n + 1];
-        Arrays.fill(memo, -1);
+        hash.put(1, 1);
+        hash.put(2, 2);
         return dp(n);
     }
 
     int dp(int n){
         if(n == 1) return 1;
         if(n == 2) return 2;
-
-        if(memo[n] == -1){
-            memo[n] = dp(n - 1) + dp(n - 2);
+        if(!hash.containsKey(n)){
+            hash.put(n, dp(n - 1) + dp(n - 2));
         }
-
-        return memo[n];
+        return hash.get(n);
     }
 }
