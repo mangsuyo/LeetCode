c37a0fd34e75c4707c390e3aab4a7bcfe1484b8d
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2026-03-14 23:05:11 +0900

    Time: 5 ms (81.46%), Space: 46.9 MB (5.55%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,46 +1,25 @@
-import java.util.*;
-
 class Solution {
-	public int longestValidParentheses(String s) {
-		int answer = 0;
-
-		int[] result = new int[s.length()];
-
-		int count = 0;
-		Deque<List<String>> stack = new ArrayDeque<>();
-
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == ')') {
-				if (!stack.isEmpty() && stack.peek().get(1).equals(")")) {
-					List<String> info = stack.pop();
-					int index = Integer.parseInt(info.get(0));
-					result[i] = 1;
-					result[index] = 1;
-				} else {
-					result[i] = -1;
-				}
-			} else {
-				stack.push(List.of(String.valueOf(i), ")"));
-			}
-		}
-
-		while(!stack.isEmpty()) {
-			List<String> info = stack.pop();
-			int index = Integer.parseInt(info.get(0));
-			result[index] = -1;
-		}
-
-
-		for(int i =0; i < result.length; i++){
-			if(result[i] == -1){
-				count = 0;
-			}
-			else{
-				count += 1;
-				if(answer < count) answer = count;
-			}
-		}
-
-		return answer;
-	}
+    int n;
+    public int longestValidParentheses(String s) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        this.n = s.length();
+        int answer = 0;
+        stack.push(-1);
+        for(int i = 0; i < n; i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(i);
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }
+                else{
+                    answer = Math.max(answer, i - stack.peek());
+                }
+            }
+        }
+        return answer;
+    }
 }
